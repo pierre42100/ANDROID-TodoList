2c37ecc1dcfc2f94a9c7afa2edb7595c49fdc778
Pierre <pierre@hubert.fr>
2017-10-25 16:31:36 +0200

    New tasks are now saved in JSON file

--- app/src/main/java/org/communiquons/android/todolist/MainActivity.java
+++ app/src/main/java/org/communiquons/android/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package org.communiquons.android.todolist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     void refresh_tasks_list(){
 
         //Get the tasks list
-        tmanag.get_all();
+        Log.v("MainActivity Debug", tmanag.get_all().toString());
 
         //DEVELOPEMENT STAGE
         //Tasks are hard-coded, but it is temporary
--- app/src/main/java/org/communiquons/android/todolist/TasksManagement.java
+++ app/src/main/java/org/communiquons/android/todolist/TasksManagement.java
@@ -3,6 +3,10 @@ package org.communiquons.android.todolist;
 import android.content.Context;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Tasks management
  *
@@ -17,11 +21,16 @@ class TasksManagement {
     private Utilities utils;
 
     /**
+     * Default content of the tasks JSON file
+     */
+    private String defaultFileContent = "{tasks:[]}";
+
+    /**
      * Task management constructor
      *
      * @param context The context where the task is being added
      */
-    public TasksManagement(Context context){
+    TasksManagement(Context context){
         //Create utilities object
         utils = new Utilities(context);
     }
@@ -29,15 +38,83 @@ class TasksManagement {
     /**
      * Get the tasks list
      *
-     * @return
+     * @return The list of tasks, as a JSONArray
      */
-    public void get_all(){
+    JSONArray get_all(){
         //Check if the tasks list exist or not
         if(!utils.file_exists("tasks.json")){
-            utils.file_put_contents("tasks.json", "{}");
+            reset_tasks_file();
         }
 
+        JSONObject file_json = new JSONObject();
 
+        //Try to decode JSON array
+        try {
+            file_json = new JSONObject(utils.file_get_contents("tasks.json"));
+        }
+        catch (JSONException e){
+            //Display error
+            e.printStackTrace();
+
+            //Reset JSON file
+            reset_tasks_file();
+        }
+
+        //Check for the task list
+        if(!file_json.has("tasks")){
+            Log.e("TasksManagement", "Tasks object not found ! Need to recreate file");
+            reset_tasks_file();
+            return new JSONArray();
+        }
+
+        //Returns tasks lis
+        try{
+            return file_json.getJSONArray("tasks");
+        }
+        catch (JSONException e){
+
+            Log.e("TasksManagement", "Couldn't extract tasks array! Need to recreate file");
+            e.printStackTrace();
+
+            //Reset JSON file
+            reset_tasks_file();
+
+            //Return empty list
+            return new JSONArray();
+        }
+    }
+
+    /**
+     * Saves a new tasks list
+     *
+     * @param taskslist The new list of tasks
+     * @return true in case of success
+     */
+    private boolean save_new_tasks_list(JSONArray taskslist){
+
+        //Generate new JSON file
+        JSONObject newfile = new JSONObject();
+
+        try {
+            newfile.put("tasks", taskslist);
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+
+            //It is a failure
+            return false;
+        }
+
+        //Export the JSON object to a string
+        String new_file_content = newfile.toString();
+
+        //Save the new file
+        if(!utils.file_put_contents("tasks.json", new_file_content)) {
+            return false;
+        }
+
+        //Else it is a success
+        return true;
     }
 
     /**
@@ -46,11 +123,37 @@ class TasksManagement {
      * @param task_name The name of the created task
      * @return False in case of failure
      */
-    public boolean create_task(String task_name){
+    boolean create_task(String task_name){
 
-        Log.v("TodoList debug", utils.file_get_contents("tasks.json"));
+        //Get the current list of tasks
+        JSONArray tasksList = get_all();
 
+        //Create the new task
+        JSONObject newTask = new JSONObject();
+        try {
+            newTask.put("name", task_name);
+            newTask.put("done", false);
+        } catch (JSONException e){
+            e.printStackTrace();
 
-        return false;
+            //This is a failure
+            return false;
+        }
+
+        //Append the new task to the list
+        tasksList.put(newTask);
+
+        //Save the new tasks list
+        save_new_tasks_list(tasksList);
+
+        //In case of success
+        return true;
+    }
+
+    /**
+     * Reset tasks file to its default values
+     */
+    private void reset_tasks_file(){
+        utils.file_put_contents("tasks.json", defaultFileContent);
     }
 }
